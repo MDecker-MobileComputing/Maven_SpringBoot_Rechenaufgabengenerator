3b0e49fedbf9e97ef4fe781ebf9ee6235df56ce5
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-07-20 09:30:03 +0200

    Aufgaben jetzt 5-stellig

--- src/main/java/de/eldecker/spring/rechenaufgabengenerator/KonsoleRunner.java
+++ src/main/java/de/eldecker/spring/rechenaufgabengenerator/KonsoleRunner.java
@@ -46,9 +46,9 @@ public class KonsoleRunner implements CommandLineRunner {
         try {
 
             final RechenaufgabenSpec spec = new RechenaufgabenSpec(
-            		1_050, 9_050,  // zahl1 im Bereich 1.050 bis 9.050
-                      120,   900,  // zahl2 im Bereich   120 bis   900
-                              72   // 72 Aufgaben => knapp 2 Seiten
+            	   10_050, 99_050,  // zahl1 im Bereich 1.050 bis 9.050
+                    1_120,  9_900,  // zahl2 im Bereich   120 bis   900
+                               72   // 72 Aufgaben => knapp 2 Seiten
             );
 
             final ByteArrayOutputStream pdfStream = 
